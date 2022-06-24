158350e2277e77b930afc477a26b239a443927d6
vaca22 <wsmslgh@gmail.com>
2022-06-24 23:15:44 +0800

    sdfgasdf

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -23,14 +25,28 @@ public class HelloController {
     public String hello2(InputStream dataStream) throws IOException {
         int s=0;
         int a=0;
+        File myObj = new File("filename.txt");
+        if(myObj.exists()){
+            myObj.delete();
+        }
+
+        FileOutputStream out=new FileOutputStream("filename.txt");
+
+
         while (true){
             a=dataStream.read(fuck);
             if(a>0){
+                if(a==100){
+                    out.write(fuck);
+                }else{
+                    out.write(fuck,0,a);
+                }
                 s+=a;
             }else{
                 break;
             }
         }
+        out.close();
         System.out.println(s);
 
         return "Hello W22orld!";
