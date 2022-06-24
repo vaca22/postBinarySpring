e84bc1bd3381c5e32f566c42726e5ac979396e65
vaca22 <wsmslgh@gmail.com>
2022-06-24 23:05:56 +0800

    sdfg

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class HelloController {
@@ -10,5 +14,25 @@ public class HelloController {
     @RequestMapping("/")
     public String hello() {
         return "Hello World!";
+    }
+
+    byte[] fuck=new byte[100];
+
+    @ResponseBody
+    @PostMapping("/post")
+    public String hello2(InputStream dataStream) throws IOException {
+        int s=0;
+        int a=0;
+        while (true){
+            a=dataStream.read(fuck);
+            if(a>0){
+                s+=a;
+            }else{
+                break;
+            }
+        }
+        System.out.println(s);
+
+        return "Hello W22orld!";
     }
 }
